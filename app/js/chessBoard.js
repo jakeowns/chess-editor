5fc99b0076d87ec788b7f06e10f3bb4af50118fa
jakeowns <beanshield@gmail.com>
2016-01-31 15:54:44 -0800

    Chess board, load FEN notation.

--- app/js/chessBoard.js
+++ app/js/chessBoard.js
@@ -1,7 +1,27 @@
 define(['jquery'], function($) {
+  //var startFen = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR';
+  var startFen = 'rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R';
   return {
     run: function() {
-      $('body').append('appendbody');
+      var rowPos = 0;
+      var rows = startFen.split('/');
+      $('board board-row').each(function() {
+        var c = rows[rowPos].split(''),
+            $squares = $(this).find('square'),
+            chp = 0;
+        for(var i = 0; i < $squares.length; i++) {
+          if(!isNaN(+c[chp])) {
+            i = i + (+c[chp]-1);
+          } else {
+            var prefix = c[chp] === c[chp].toUpperCase() ? '_' : '';
+            $($squares.get(i)).append(
+              $('<piece>').append('<img src="/app/img/pieces/'+ prefix + c[chp] +'.png"/>')
+            );
+          }
+          chp++;
+        }
+        rowPos++;
+      });
     }
   };
 });
