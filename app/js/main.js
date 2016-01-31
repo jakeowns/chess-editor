35b4e2ff21e6eaf3f68d84d17e893a7e85577d89
jakeowns <beanshield@gmail.com>
2016-01-31 13:24:21 -0800

    Init commit.

---
+++ app/js/main.js
@@ -0,0 +1,17 @@
+require.config({
+  baseUrl: "/app/js",
+  shim : {
+    "bootstrap" : { "deps" :['jquery'] }
+  },
+  paths: {
+    "jquery" : "/bower/jquery/dist/jquery.min",
+    "bootstrap" :  "/bower/bootstrap/dist/js/bootstrap.min"
+  }
+});
+
+require(['jquery', 'bootstrap', 'chessBoard'], function($, boostrap, chessBoard) {
+  $(function() {
+    console.log('dom.ready');  // dom.ready
+    chessBoard.run();
+  });
+});
