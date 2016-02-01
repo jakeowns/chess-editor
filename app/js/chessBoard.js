8aada1769980a1fcb9a1f622d4c32e49ca1ab24c
jakeowns <beanshield@gmail.com>
2016-01-31 17:11:48 -0800

    Add draggable, include piece images.

--- app/js/chessBoard.js
+++ app/js/chessBoard.js
@@ -1,10 +1,20 @@
-define(['jquery'], function($) {
+define(['jquery', 'jqueryui'], function($) {
   //var startFen = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR';
-  var startFen = 'rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R';
+  var startFen = 'rnbqkb1r/1p2pppp/p2p1n2/8/3NP3/2N5/PPP2PPP/R1BQKB1R';
   return {
     run: function() {
       var rowPos = 0;
       var rows = startFen.split('/');
+      $('square').droppable({
+        drop: function(e, ui) {
+          var $drag = $(ui.draggable).detach().css({ top: 0, left: 0 });
+          $(this).empty();
+          $(this).append( $drag );
+          setTimeout(function() {
+            $drag.draggable();
+          }, 500);
+        }
+      });
       $('board board-row').each(function() {
         var c = rows[rowPos].split(''),
             $squares = $(this).find('square'),
@@ -15,7 +25,11 @@ define(['jquery'], function($) {
           } else {
             var prefix = c[chp] === c[chp].toUpperCase() ? '_' : '';
             $($squares.get(i)).append(
-              $('<piece>').append('<img src="/app/img/pieces/'+ prefix + c[chp] +'.png"/>')
+              $('<piece>')
+                .draggable({
+                  revert: 'invalid'
+                })
+                .append('<img src="/app/img/pieces/'+ prefix + c[chp] +'.png"/>')
             );
           }
           chp++;
--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,12 @@
 require.config({
   baseUrl: "/app/js",
   shim : {
-    "bootstrap" : { "deps" :['jquery'] }
+    "bootstrap" : { "deps": ["jquery"] },
+    "jqueryui" : { "deps": ["jquery"] }
   },
   paths: {
     "jquery" : "/bower/jquery/dist/jquery.min",
+    "jqueryui" : "/bower/jqueryui/jquery-ui.min",
     "bootstrap" :  "/bower/bootstrap/dist/js/bootstrap.min"
   }
 });
