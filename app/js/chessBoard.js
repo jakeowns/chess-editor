da2dcdedc43e8c67997b232e45dc24da97a58dcc
jakeowns <beanshield@gmail.com>
2016-01-31 18:24:51 -0800

    Refactor code to fenLoader.

--- app/js/chessBoard.js
+++ app/js/chessBoard.js
@@ -1,41 +1,24 @@
-define(['jquery', 'jqueryui'], function($) {
+define(['jquery', 'jqueryui', 'fenLoader'], function($, ui, fenLoader) {
   //var startFen = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR';
   var startFen = 'rnbqkb1r/1p2pppp/p2p1n2/8/3NP3/2N5/PPP2PPP/R1BQKB1R';
   return {
     run: function() {
-      var rowPos = 0;
-      var rows = startFen.split('/');
-      $('square').droppable({
-        drop: function(e, ui) {
-          var $drag = $(ui.draggable).detach().css({ top: 0, left: 0 });
-          $(this).empty();
-          $(this).append( $drag );
-          setTimeout(function() {
-            $drag.draggable();
-          }, 500);
-        }
-      });
-      $('board board-row').each(function() {
-        var c = rows[rowPos].split(''),
-            $squares = $(this).find('square'),
-            chp = 0;
-        for(var i = 0; i < $squares.length; i++) {
-          if(!isNaN(+c[chp])) {
-            i = i + (+c[chp]-1);
-          } else {
-            var prefix = c[chp] === c[chp].toUpperCase() ? '_' : '';
-            $($squares.get(i)).append(
-              $('<piece>')
-                .draggable({
-                  revert: 'invalid'
-                })
-                .append('<img src="/app/img/pieces/'+ prefix + c[chp] +'.png"/>')
-            );
+      fenLoader.load(startFen);
+      // init drag events
+      $('board')
+        .find('square').droppable({
+          drop: function(e, ui) {
+            var $drag = $(ui.draggable).detach().css({ top: 0, left: 0 });
+            $(this).empty().append( $drag );
+            setTimeout(function() {
+              $drag.draggable();
+            }, 500);
           }
-          chp++;
-        }
-        rowPos++;
-      });
+        }).find('piece').draggable({
+          zIndex: 10,
+          containment: 'board',
+          scroll: false
+        });
     }
   };
 });
---
+++ app/js/fenLoader.js
@@ -0,0 +1,24 @@
+define(['jquery'], function($) {
+  //reg /([rnbqkn1-8]{1,8}\/?){8}/i
+  return {
+    load: function(fen) {
+      var rows = fen.split('/');
+      $('board board-row').each(function() {
+        var c = rows[ $(this).index() ].split(''),
+            $squares = $(this).find('square'),
+            chp = 0;
+        for(var i = 0; i < $squares.length; i++) {
+          if(!isNaN(+c[chp])) {
+            i = i + (+c[chp]-1);
+          } else {
+            var prefix = c[chp] === c[chp].toUpperCase() ? '_' : '';
+            $($squares.get(i)).append(
+              $('<piece>').append('<img src="/app/img/pieces/' + prefix + c[chp] + '.png"/>')
+            );
+          }
+          chp++;
+        }
+      });
+    }
+  };
+});
